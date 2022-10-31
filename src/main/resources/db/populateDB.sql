859a430f0fce1cb8f4efe68a206edfb8e1e0fd1c
Artem Zhuravlev <enigma.mrgreen@gmail.com>
2022-10-31 16:55:06 +0100

    realized JdbcMealRepository, fix bug for InMemory Controller and SpringMain

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -10,3 +11,15 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (description, datetime, calories, user_id)
+VALUES ('breakfast User', '2022-10-29 07:00', 500, 100000),
+       ('lunch User', '2022-10-29 13:00', 1500, 100000),
+       ('dinner User', '2022-10-29 17:00', 1000, 100000),
+       ('night snack User', '2022-10-29 00:00', 600, 100000),
+       ('breakfast 2 User', '2022-10-30 10:00', 500, 100000),
+       ('lunch User', '2022-10-30 15:00', 1100, 100000),
+       ('dinner User', '2022-10-30 20:00', 300, 100000),
+       ('breakfast Admin', '2022-10-25 08:00', 300, 100001),
+       ('lunch Admin', '2022-10-25 12:00', 1000, 100001),
+       ('dinner Admin', '2022-10-25 19:00', 800, 100001);
